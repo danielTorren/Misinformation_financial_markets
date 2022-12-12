fixed switching in weighting matrix

plot_results.py
@@ -110,13 +110,14 @@ def plot_time_series_consumer_triple(fileName,Data,y_title,dpi_save,property_y,n
 
     for i, ax in enumerate(axes.flat):
         for v in range(len(Data.agent_list)):
-            if (Data.agent_list[v].history_c_bool[0]):
-                color = "blue"
-            else:
-                color = "red"
+            # if (Data.agent_list[v].history_c_bool[0]):
+            #     color = "blue"
+            # else:
+            #     color = "red"
             data_ind = np.asarray(  eval("Data.agent_list[%s].%s" % (str(v), property_y))   )#get the list of data for that specific agent
             #print(data_ind[:, i])
-            ax.plot(np.asarray(Data.history_time), data_ind[:, i], color = color)#plot the ith column in the weighting matrix which is [T x num_signals] where T is total steps
+            # ax.plot(np.asarray(Data.history_time), data_ind[:, i], color = color)
+            ax.plot(np.asarray(Data.history_time), data_ind[:, i])#plot the ith column in the weighting matrix which is [T x num_signals] where T is total steps
         ax.set_xlabel("Steps")
         #ax.set_ylim([0,1])
         ax.set_ylabel("%s" % y_title)
@@ -343,13 +344,15 @@ def plot_time_series_consumer_triple_multi(fileName,Data_list,y_title,dpi_save,p
 
     for i, ax in enumerate(axes.flat):
         for v in range(len(Data_list[i].agent_list)):
-            if (Data_list[i].agent_list[v].history_c_bool[0]):
-                color = "blue"
-            else:
-                color = "red"
+            #if (Data_list[i].agent_list[v].history_c_bool[0]):
+            #    color = "blue"
+            #else:
+            #    color = "red"
             data_ind = np.asarray(eval("Data_list[%s].agent_list[%s].%s" % (str(i),str(v), property_y)))#get the list of data for that specific agent
             #print(data_ind[:, i])
-            ax.plot(np.asarray(Data_list[i].history_time), data_ind[:, signal], color = color)#plot the ith column in the weighting matrix which is [T x num_signals] where T is total steps
+            ax.plot(np.asarray(Data_list[i].history_time), data_ind[:, signal])#plot the ith column in the weighting matrix which is [T x num_signals] where T is total steps
+            #ax.plot(np.asarray(Data_list[i].history_time), data_ind[:, signal], color = color)#plot the ith column in the weighting matrix which is [T x num_signals] where T is total steps
+        
         ax.set_xlabel("Steps")
         #ax.set_ylim([0,1])
         
@@ -454,7 +457,7 @@ if __name__ == "__main__":
         start_time = time.time()
 
     if single_shot:
-        fileName = "results/single_shot_21_56_21__11_12_2022"#"results/single_shot_steps_500_I_100_network_structure_small_world_degroot_aggregation_1"
+        fileName = "results/single_shot_12_29_28__12_12_2022"#"results/single_shot_steps_500_I_100_network_structure_small_world_degroot_aggregation_1"
         createFolder(fileName)
         Data = load_object(fileName + "/Data", "financial_market")
         base_params = load_object(fileName + "/Data", "base_params")
@@ -466,8 +469,8 @@ if __name__ == "__main__":
         plot_history_profit = plot_time_series_consumers(fileName,Data,"Profit",dpi_save,"history_profit",red_blue_c)
         #plot_history_lambda_t = plot_time_series_consumers(fileName,Data,"Network signal, $\lambda_{t,i}$",dpi_save,"history_lambda_t",red_blue_c)
         ##
-        plot_history_expectation_theta_mean = plot_time_series_consumers(fileName,Data,"Expectation mean, $E(\mu_{\theta})$",dpi_save,"history_expectation_theta_mean",red_blue_c)
-        plot_history_expectation_theta_variance = plot_time_series_consumers(fileName,Data,"Expectation variance, $E(\sigma_{\theta}^2)$",dpi_save,"history_expectation_theta_variance",red_blue_c)
+        #plot_history_expectation_theta_mean = plot_time_series_consumers(fileName,Data,"Expectation mean, $E(\mu_{\theta})$",dpi_save,"history_expectation_theta_mean",red_blue_c)
+        #plot_history_expectation_theta_variance = plot_time_series_consumers(fileName,Data,"Expectation variance, $E(\sigma_{\theta}^2)$",dpi_save,"history_expectation_theta_variance",red_blue_c)
 
         #consumer X list and weighting
         ##plot_history_demand = plot_time_series_consumer_triple(fileName,Data,"Theoretical whole demand, $X_k$",dpi_save,"history_theoretical_X_list", 3, ["$X_{\theta}$", "$X_{\zeta}$", "$X_{\lambda}$"],red_blue_c)
@@ -480,7 +483,7 @@ if __name__ == "__main__":
         plot_history_informed_proportion = plot_time_series_market(fileName,Data,"Informed prop.",dpi_save,"history_informed_proportion")  
         #plot_history_d_t = plot_time_series_market(fileName,Data,"Dividend ,$d_t$",dpi_save,"history_d_t")
         ##plot_history_zeta_t = plot_time_series_market(fileName,Data,"$S_{\omega}$",dpi_save,"zeta_t")
-        plot_network_c = plot_network_shape(fileName, Data, base_params["network_structure"], "c bool","history_c_bool",cmap, norm_zero_one, node_size,dpi_save)
+        #plot_network_c = plot_network_shape(fileName, Data, base_params["network_structure"], "c bool","history_c_bool",cmap, norm_zero_one, node_size,dpi_save)
         ##plot_history_pulsing = plot_time_series_market_pulsing(fileName,Data,"$In phase?$",dpi_save)
         #plot_degree_distribution = degree_distribution_single(fileName,Data,dpi_save)
         #plot_weighting_matrix_relations = plot_line_weighting_matrix(fileName,Data,dpi_save)